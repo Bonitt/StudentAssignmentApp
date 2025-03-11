844b280244f5d51520ed4b5f51b7fcd27147021d
Bonitt <matthewbonett1309@gmail.com>
2025-03-11 18:22:57 +0000

    Created Student and College Models

---
+++ app/Models/College.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class College extends Model
+{
+
+    protected $fillable = ['name', 'address'];
+
+
+    use HasFactory;
+
+    public function students()
+    {
+        return $this->hasMany(Student::class);
+    }
+}
